180fad7765b0e94096f2aaa57775c1cb48353e27
MikeFortKnox <134332447+MikeFortKnox@users.noreply.github.com>
2024-11-14 12:05:08 -0500

    inital commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const { PORT = 3001 } = process.env;
+const express = require("express");
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}...`);
+});
